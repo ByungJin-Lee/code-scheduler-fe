d448ff7b346b45d3d95620939a85cd1a9ced71df
byungjin <byungjin.dev@gmail.com>
2023-01-08 01:14:01 +0900

    chore: Edit Background Style & Add Context Provider(#5)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ThemeProvider from "../components/ThemeProvider";
 import ThemeCSSWrap from "../components/ThemeCSSWrap";
+import LanguageProvider from "../components/LanguageProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <ThemeCSSWrap>
-        <Component {...pageProps} />
-      </ThemeCSSWrap>
-    </ThemeProvider>
+    <LanguageProvider>
+      <ThemeProvider>
+        <ThemeCSSWrap>
+          <Component {...pageProps} />
+        </ThemeCSSWrap>
+      </ThemeProvider>
+    </LanguageProvider>
   );
 }
